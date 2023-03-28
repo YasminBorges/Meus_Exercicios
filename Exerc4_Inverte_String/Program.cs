b81b3268927fb53c445e6cfe891094bf14acf603
YasminBorges <73182977+YasminBorges@users.noreply.github.com>
2023-03-28 20:01:37 -0300

    Delete Exerc4_Inverte_String directory

--- Exerc4_Inverte_String/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-string texto = Console.ReadLine();
-int cont = texto.Count();
-
-for (int i = cont; i > 0; i--)
-{
-    Console.WriteLine(texto);
-}
