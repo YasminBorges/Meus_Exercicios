e198d1ed47aba1f8e3d6c148230c5c013b95c452
YasminBorges <73182977+YasminBorges@users.noreply.github.com>
2023-03-28 20:00:10 -0300

    Add files via upload

---
+++ Exerc4_Inverte_String/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string texto = Console.ReadLine();
+int cont = texto.Count();
+
+for (int i = cont; i > 0; i--)
+{
+    Console.WriteLine(texto);
+}
